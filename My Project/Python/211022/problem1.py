8450ce40defe8f9dff2fdcd167fa3a59b876aab2
C-H2Baek <neptune8509@gmail.com>
2021-10-22 14:51:47 +0900

    오후확인

---
+++ My Project/Python/211022/problem1.py
@@ -0,0 +1,30 @@
+#리스트내의 특정값이 몇 번쨰인가 순차탐색을 통한 검색 값 찾기
+
+colors= [ 'red' , 'blue' , 'green']
+colors[0]
+
+
+# 학생 정보의 리스트 표현
+
+number_list= input("Choose Number :  (split,)"),split(',')
+
+while True:
+
+    print(number_list)
+    
+    a=int(input("Search number In List : "))
+    b=0;
+    
+    for i in range(0,6) :
+        
+        if a==list1[i]:
+            b=i+1
+            print("%dis %dLocation." %(a,b))
+
+
+# other
+
+
+
+
+
