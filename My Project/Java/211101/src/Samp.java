cf57f4ac5c34848d5a03f954e008e3a6d8862c32
Chan Hyeon Baek <C-H2Baek>
2021-11-01 11:02:44 +0900

    Si ba

---
+++ My Project/Java/211101/src/Samp.java
@@ -0,0 +1,23 @@
+public class Samp {
+
+	public static void main(String[] args) {
+		int[] answer = {1,1,2,2,2,3,3,3,3,4,4,4,4,4 };
+		int[] counter = new int[4];
+
+		for (int i = 0; i < answer.length; i++) {
+			counter[answer[i] - 1]++;
+			//answer[0] = 1이므로 counter[1-1] = counter[0] = 0 -> 1
+			//answer[1] = 4이므로 counter[4-1] = counter[3] = 0 -> 1
+			//answer[2] = 4이므로 counter[4-1] = counter[3] = 1 -> 2
+		}
+
+		for (int i = 0; i < counter.length; i++) {
+			System.out.print(counter[i]);
+			
+			for (int j = 0; j < counter[i]; j++) 
+				System.out.print("*");				//베열에 담긴 숫자의 개수만큼 * 출력
+			
+			System.out.println();
+		}
+	}
+}
