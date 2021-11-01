253bb3aa0628a971da4af37aa0fca5327d758161
Chan Hyeon Baek <neptune8509@gmail.com>
2021-11-01 13:57:27 +0900

    ??

---
+++ My Project/Java/211101/src/SutdaCard.java
@@ -0,0 +1,29 @@
+
+
+class SutdaCard1 {
+	public static void main(String args[]) {
+		SutdaCard card1 = new SutdaCard(3, false);
+		SutdaCard card2 = new SutdaCard();
+		System.out.println(card1.info());
+		System.out.println(card2.info());
+	}
+}
+
+class SutdaCard { 
+	int num;
+	boolean isKwang;
+
+	public SutdaCard(){
+		this(1, true); //SutdaCard(1,true)를 호출한다.
+	}
+
+	public SutdaCard(int num, boolean isKwang){
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+
+	public String info(){
+		return num + (isKwang ? "K" : "");
+	}
+}
+
