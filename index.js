827fb0e9c6939fd1d43244c1ffe2491e3f055516
Chan Hyeon Baek <neptune8509@gmail.com>
2021-11-15 16:41:46 +0900

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const a_collection = document.getElementsByTagName('a')
 Array.from(a_collection).forEach(a => {
-    a.addEventListener('click', function(){
+    a.addEventListener('click', function() {
         alert(this.firstElementChild.alt)
     })
 });
