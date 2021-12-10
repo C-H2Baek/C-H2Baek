6d6ab241f664f3bf181a5dd6429cfa23ff95e6c7
C-H2Baek <neptune8509@gmail.com>
2021-12-10 15:44:51 +0900

    Commit

---
+++ My Project/Java/211209/src/HashMapScoreEx.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class HashMapScoreEx {
+	public static void main(String[] args) {
+		// 이름과 점수를 저장할 HashMap 컬렉션 생성
+		HashMap<String, Integer> scoreMap = new HashMap<String, Integer>();
+			// var scoreMap = new HashMap<String, Inte
+	}
+}
